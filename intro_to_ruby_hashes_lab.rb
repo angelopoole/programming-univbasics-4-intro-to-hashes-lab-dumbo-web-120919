5e045698ca232a88326434372eb988377a36bf38
Angelo Poole <angelo.poole@outlook.com>
2019-12-05 22:58:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,7 @@ end
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   # Hash.new { |value, key| value[key] =  }
-  hash {
-    "#{key}" => value
-  }
+  {key => value}
 end
 
 def read_from_hash(hash, key)
