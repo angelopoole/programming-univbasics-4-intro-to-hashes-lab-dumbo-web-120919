86da9ef42896983341d4268d3a087b4fc6120197
Angelo Poole <angelo.poole@outlook.com>
2019-12-05 22:48:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,7 @@ end
 def my_hash
   # return a valid hash with any key/value pair of your choice
   newhash = {
-    :haloReach => "on pc"
+    :haloReach => "on pc",
     :halo5 => "not on pc yet"
   }
 end
